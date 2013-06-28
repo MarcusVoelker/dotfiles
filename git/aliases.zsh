96b04fe5c63eea2e6b5bc41527a832db668df3c0
Neth Shady <neth.shady@googlemail.com>
2013-06-28 15:55:52 +0200

    glp command and vim stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,6 @@
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias glp='git pull origin $(current_branch) && git push origin $(current_branch)'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
